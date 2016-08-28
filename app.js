9dd7640485bf0d8af9d690894e471acceaf4ddb8
Mathias Schilling <m@matchilling.com>
2016-08-28 02:43:28 +0100

    This is where it all begins ... :rocket:

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+module.exports = app; // for testing
+
+var config = {
+    appRoot: __dirname // required config
+};
+
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+
+    // install middleware
+    swaggerExpress.register(app);
+
+    var port = process.env.PORT || 8080;
+    app.listen(port);
+
+    if (swaggerExpress.runner.swagger.paths['/hello']) {
+        console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+    }
+});
